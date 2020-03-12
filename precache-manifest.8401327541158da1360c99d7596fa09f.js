f73cc42054820bca591462e2e9881acc70aa5726
Ma Diallo <saylujalloh@gmail.com>
2020-03-12 14:58:46 -0400

    Updates

---
+++ precache-manifest.8401327541158da1360c99d7596fa09f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fbc39b76f052be9d9af",
+    "url": "/jalloh-codes/neightborhoodmap.io/static/css/main.89810ffc.chunk.css"
+  },
+  {
+    "revision": "4fbc39b76f052be9d9af",
+    "url": "/jalloh-codes/neightborhoodmap.io/static/js/main.4fbc39b7.chunk.js"
+  },
+  {
+    "revision": "f5c41f4d38db9e6d7f9c",
+    "url": "/jalloh-codes/neightborhoodmap.io/static/js/1.f5c41f4d.chunk.js"
+  },
+  {
+    "revision": "d52f1b293b8c1034b0b2",
+    "url": "/jalloh-codes/neightborhoodmap.io/static/js/runtime~main.d52f1b29.js"
+  },
+  {
+    "revision": "ef45afcf547c62f79b663fc53dfa8d99",
+    "url": "/jalloh-codes/neightborhoodmap.io/index.html"
+  }
+];
